e711693705f0328deb369b747d8485f409a7564f
Ken Egawa <208090838+NekawageUW@users.noreply.github.com>
2025-05-24 14:43:48 -0700

    App pages.tsx created

---
+++ src/app/books/[isbn]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BookSingle from 'views/books/book-single';
+
+// ==============================|| PAGE ||============================== //
+
+export default function BookSinglePage() {
+  return <BookSingle />;
+}
