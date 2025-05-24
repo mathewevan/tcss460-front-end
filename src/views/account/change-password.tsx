8973b55b1801b0e08301269db1bdd93bcf7ad6e9
Mathew Miller <mathew.evan85@gmail.com>
2025-05-24 12:16:13 -0700

    This allows a user to register using group 2 API and includes all necessary requirements. Also contains the foundation on the change password page/form to be completed at a later date.

---
+++ src/views/account/change-password.tsx
@@ -0,0 +1,25 @@
+// material-ui
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+// project import
+import AccountChangePassword from 'sections/account/account-forms/ChangePassword';
+
+// ================================|| CHANGE PASSWORD ||================================ //
+
+export default function ChangePassword() {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Stack sx={{ mb: { xs: -0.5, sm: 0.5 } }} spacing={1}>
+          <Typography variant="h3">Change Password</Typography>
+          <Typography color="secondary">Please choose your new password</Typography>
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <AccountChangePassword />
+      </Grid>
+    </Grid>
+  );
+}
