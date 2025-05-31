b842a74a338833ea45113e2338ae08f55f9dc547
kylieh74 <155610861+kylieh74@users.noreply.github.com>
2025-05-31 15:10:57 -0700

    Create page.tsx

---
+++ src/app/books/create/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CreateBook from '../../../views/books/create-book';
+
+// ==============================|| PAGE ||============================== //
+
+export default function BookSinglePage() {
+  return <CreateBook />;
+}
