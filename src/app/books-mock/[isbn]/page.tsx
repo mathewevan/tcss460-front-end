644a6183004de435a121ec3557544d407c7ca2f8
Ken Egawa <208090838+NekawageUW@users.noreply.github.com>
2025-06-01 06:37:05 -0700

    UI changes, mock version created
    Created book-single-mock to visualize ui changes without being hooked up.

---
+++ src/app/books-mock/[isbn]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BookSingle from '../../../views/books/book-single-mock';
+
+// ==============================|| PAGE ||============================== //
+
+export default function BookSinglePage() {
+  return <BookSingle />;
+}
