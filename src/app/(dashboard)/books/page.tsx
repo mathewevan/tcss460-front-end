9a2c005f858f96ae7597a20b09b2d88150ec38dd
Mathew Miller <mathew.evan85@gmail.com>
2025-06-03 14:07:25 -0700

    Minor fixes

--- src/app/(dashboard)/books/page.tsx
+++ src/app/(dashboard)/books/page.tsx
@@ -3,6 +3,7 @@ import BookList from '../../../views/books/book-list';
 
 // ==============================|| PAGE ||============================== //
 
-export default function BookListPage() {
-  return <BookList />;
+export default function BookListPage({ params }: { params: { title: string } }) {
+  const { title } = params;
+  return <BookList title={title} />;
 }
